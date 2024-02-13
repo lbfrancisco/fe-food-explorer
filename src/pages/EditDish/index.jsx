3334df93484b8ba0c24eab7824bc0c2f9ce1a20b
lbfrancisco <lucasbarroso318@gmail.com>
2024-02-13 15:06:35 -0300

    feat: add update dish function

--- src/pages/EditDish/index.jsx
+++ src/pages/EditDish/index.jsx
@@ -7,15 +7,13 @@ import TagItem from "../../components/TagItem";
 import { api } from "../../services/api";
 
 export default function EditDish() {
-  const [ingredients, setIngredients] = useState([]);
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [ingredients, setIngredients] = useState([]);
   const [currentIngredient, setCurrentIngredient] = useState('');
-  const [price, setPrice] = useState(0);
-  const [image, setImage] = useState('');
   const [category, setCategory] = useState('#');
-
-  const [categories, setCategories] = useState([]);
+  const [image, setImage] = useState('');
 
   const params = useParams();
   const navigate = useNavigate();
@@ -25,39 +23,50 @@ export default function EditDish() {
       const { id } = params;
 
       try {
-        const categoryResponse = await api.get('/categories');
-        const { categories } = categoryResponse.data;
-
-        setCategories(categories);
-
         const dishResponse = await api.get(`/dishes/${id}`);
         const { dishes } = dishResponse.data;
 
-        dishes.map((dish) => {
-          setName(dish.name);
-          setDescription(dish.description);
-          setPrice(dish.price.replace(',', '.'));
-          setIngredients(dish.ingredients_list.split(','));
+        console.log(dishes[0]);
 
-          if (categories.length > 0) {
-            const categoryMatch = categories.find((category) => category.name === dish.category_name);
-            setCategory(categoryMatch.id);
-          }
+        setName(dishes[0].name);
+        setDescription(dishes[0].description);
+        setCategory(getCategoryIdByName(dishes[0].category_name));
+        setPrice(dishes[0].price.replace(',', '.'));
 
+        if (dishes[0].ingredients_list.length > 0) {
+          const ingredientsArray = dishes[0].ingredients_list.split(',');
+          const formattedIngredients = ingredientsArray.map(ingredient => {
+            return { name: ingredient.trim() };
+          });
 
-        });
-      } catch {
+          setIngredients(formattedIngredients);
+        }
+      } catch(error) {
         alert(`Erro ao obter informações.`);
+        console.log(error);
       }
     }
 
     loadDish();
   }, []);
 
+  function getCategoryIdByName(name) {
+  switch (name) {
+    case 'Refeições':
+      return '1';
+    case 'Sobremesas':
+      return '2';
+    case 'Bebidas':
+      return '3';
+    default:
+      return '#';
+  }
+  }
+
   function handleAddIngredientTag() {
     if (!currentIngredient) return;
 
-    const newIngredientsArray = [...ingredients, currentIngredient];
+    const newIngredientsArray = [...ingredients, { name: currentIngredient }];
     setIngredients(newIngredientsArray);
     setCurrentIngredient('');
   }
@@ -67,15 +76,42 @@ export default function EditDish() {
     setIngredients(newIngredientsArray);
   }
 
-  function handleSubmit() {
+  async function handleSubmit() {
+    if (!name || !description || ingredients.length === 0 || !price || !category) {
+      return alert('Por favor, preencha todos os campos digitáveis!');
+    }
 
+    const fileFormData = new FormData();
+    fileFormData.append('image', image);
+
+    const { id } = params;
+
+    try {
+      if (!fileFormData.get('image')) {
+        await api.put(`/dishes/${id}`, {
+          name,
+          description,
+          price: price.replace('.', ','),
+          category_id: category,
+          ingredients
+        });
+
+        alert('Prato atualizado com sucesso');
+        navigate('/');
+      } else {
+        alert('com form data')
+      }
+    } catch(error) {
+      alert('Ocorreu um erro ao atualizar seu prato. Tente novamente!');
+      console.log(error);
+    }
   }
 
   async function handleDeletePlate() {
     const { id } = params;
 
     try {
-      const response = await api.delete('/dishes/' + id);
+      await api.delete(`/dishes/${id}`);
       alert('Prato deletado com sucesso!');
       navigate('/');
     } catch {
@@ -136,11 +172,9 @@ export default function EditDish() {
             onChange={(event) => setCategory(event.target.value)}
           >
             <option value="#" disabled selected>Selecione uma categoria...</option>
-            {categories && categories.map((cat) => {
-              return (
-                <option key={cat.id} value={cat.id}>{cat.name}</option>
-              );
-            })}
+            <option value="1">Refeições</option>
+            <option value="2">Sobremesas</option>
+            <option value="3">Bebidas</option>
           </select>
         </InputContainer>
       </Wrapper>
@@ -154,7 +188,7 @@ export default function EditDish() {
               return (
                 <TagItem
                   key={index}
-                  value={ingredient}
+                  value={ingredient.name}
                   onClick={() => handleRemoveIngredientTag(index)}
                 />
               )
@@ -198,7 +232,8 @@ export default function EditDish() {
       <Wrapper>
         <button
           id="save"
-          type="submit"
+          type="button"
+          onClick={handleSubmit}
         >
           Salvar alterações
         </button>
